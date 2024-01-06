89bc57aa4f755d34d1cd36190c6e205897e8d2ed
Daniel Teles Brito <dtdanielteles@gmail.com>
2024-01-06 15:27:19 -0300

    organização dos arquivos

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "sql109.epizy.com";
-$usuario = "epiz_28344500";
-$senha = "kR9cYlG4bA";
-$dbname = "epiz_28344500_celke";
-//Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-
